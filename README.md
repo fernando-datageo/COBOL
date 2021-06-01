e9ffa365a7c8b4a40dcac5144eaf760cce098f21
fernando-datageo <fernando.datageo@gmail.com>
2021-06-01 15:09:10 -0300

    Hello World
    Hello World from COBOL

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # COBOL
+
+- Hello World
  
---
+++ hello_world.cbl
@@ -0,0 +1,14 @@
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID. PRGCOB01.
+      ******************************************************
+      ***   AREA DE COMENTARIOS - REMARKS 
+      ***   OBJETIVO DO PROGRAMA = MOSTRAR A STRING HELLO WORLD
+      ***   AUTOR: LUIS FERNANDO
+      ***   DATA : 01/06/2021
+      ******************************************************      
+       ENVIRONMENT DIVISION.
+       DATA DIVISION.
+       WORKING-STORAGE SECTION.
+       PROCEDURE DIVISION.
+           DISPLAY 'Hello World'.
+           STOP RUN.
